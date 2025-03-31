4ab2eca22bad05da15f26096f569d70e45cfd5d2
Progyan Sen <progyansen@gmail.com>
2025-03-31 16:49:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "./componants/sidebar/sidebar";
+import Main from "./componants/main/main";
+const App = () => { 
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  )
+}
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  define: {
+    'process.env': process.env
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer'
+    }
+  },
+  optimizeDeps: {
+    include: ['buffer']
+  }
+})
