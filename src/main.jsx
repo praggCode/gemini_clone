b08b6a53506191915e6a4d661aa6925ad404a4fb
Progyan Sen <progyansen@gmail.com>
2025-04-29 20:48:22 +0530

    v5.03

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import ContextProvider from './context/Context.jsx'
+import App from './App'
+import ContextProvider from './context/Context'
 
 createRoot(document.getElementById('root')).render(
   <ContextProvider>
