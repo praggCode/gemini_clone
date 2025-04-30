bc5057f95e00f64cf1cf15e21cf62753f1aed7b6
Progyan Sen <progyansen@gmail.com>
2025-04-30 11:06:33 +0530

    Fixing bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import ContextProvider from './context/ContextGiver';
+
+import  ContextProvider  from './context/context';
 
 createRoot(document.getElementById('root')).render(
   <ContextProvider>
