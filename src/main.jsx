b99c7eac262488e394920a8437eff29d9fc4588e
Progyan Sen <progyansen@gmail.com>
2025-04-30 10:51:01 +0530

    Making upload ready

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
-import ContextProvider from './context/Context'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import ContextProvider from './context/contextProvider';
 
 createRoot(document.getElementById('root')).render(
   <ContextProvider>
     <App />
-  </ContextProvider>,
-)
+  </ContextProvider>
+);
